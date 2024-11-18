9ed9aa03ac8efd382ea6a1a2a8731ada1b714f63
Manivgn <manicarmelvalguena@gmail.com>
2024-11-18 20:49:36 +0100

    feat: discussion layout

--- src/user/dashboard/Message.tsx
+++ src/user/dashboard/Message.tsx
@@ -1,12 +1,23 @@
 import {Button} from "@chakra-ui/react";
 import "./Message.css";
+import {MessageBullet} from "./MessageBullet";
+import {useEffect, useRef} from "react";
 
 export const Message = () => {
+
+    const conversationRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        // Défile automatiquement vers le bas
+        if (conversationRef.current) {
+            conversationRef.current.scrollTop = conversationRef.current.scrollHeight;
+        }
+    }, []); // Défile lorsque "messages" est mis à jour
     return (
         <>
             <div className="messages">
-                <div className="conversation">
-
+                <div className="conversation" ref={conversationRef}>
+                    <MessageBullet />
                 </div>
                 <div className="message">
                     <textarea id="message" name="message" placeholder="Message"></textarea>
--- src/user/dashboard/MessageBullet.tsx
+++ src/user/dashboard/MessageBullet.tsx
@@ -2,10 +2,63 @@ import './MessageBullet.css'
 
 export const MessageBullet = () => {
     return (
-        <>
-            <div className="message-bullet">
-
-            </div>
-        </>
+        <div className="discussion">
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+            <div className="message-bullet sent">Hello! How are you?</div>
+            <div className="message-bullet received">I’m good, thanks! How about you?</div>
+        </div>
     );
 }
