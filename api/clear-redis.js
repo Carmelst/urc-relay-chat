5c3a049fb0e17b493999dfe9bf0c62ec22d0bc9b
Manivgn <manicarmelvalguena@gmail.com>
2024-11-20 22:56:15 +0100

    fix: messages database

---
+++ api/clear-redis.js
@@ -0,0 +1,21 @@
+import { Redis } from '@upstash/redis';
+
+const redis = Redis.fromEnv();
+
+export default async function handler(req, res) {
+    try {
+        // Get all keys from Redis (WARNING: avoid in production with large datasets)
+        let number = 0;
+        const keys = await redis.keys('*');
+        console.log('All keys:', keys);
+        for (const key of keys) {
+            await redis.del(key);
+            console.log(`Deleted list with key: ${key}`);
+            number += 1 ;
+        }
+        return res.json(number);
+    } catch (error) {
+        console.error('Error retrieving keys:', error);
+    }
+}
+
